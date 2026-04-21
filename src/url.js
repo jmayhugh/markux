824bf5fc7f6dec67d9bcc7179427b6f68ff115cf
Julia Mayhugh <julia.mayhugh@gmail.com>
2026-04-21 13:52:12 -0400

    widget: normalizeUrl drops all query params

--- src/url.js
+++ src/url.js
@@ -1,17 +1,8 @@
-const TRACKING_PARAMS = new Set([
-  "utm_source", "utm_medium", "utm_campaign", "utm_term", "utm_content",
-  "fbclid", "gclid", "ref",
-]);
-
 export function normalizeUrl(rawUrl) {
   const url = new URL(rawUrl);
   url.hostname = url.hostname.toLowerCase();
   url.hash = "";
-  const params = new URLSearchParams(url.search);
-  for (const key of [...params.keys()]) {
-    if (TRACKING_PARAMS.has(key)) params.delete(key);
-  }
-  url.search = params.toString();
+  url.search = "";
   let result = url.toString();
   if (result.endsWith("/") && new URL(result).pathname !== "/") {
     result = result.slice(0, -1);
